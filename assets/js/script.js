0e0d866bc9ea417914b3c4cbd22d01bb1e7d9d1f
Kayla Casale <kayla.casale@gmail.com>
2022-12-05 10:03:26 -0800

    rearranged several times to try to get object array sequentially in h2Div and answer variable through iteration

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,14 @@ var startTextEl = document.querySelector(".startQuiz");
 var startBtn = document.getElementById("startBtn");
 var formEl = document.querySelector("form");
 formEl.style.display = "none";
-
+var submitBtn;
 
 function startQuiz() {
     startBtn.addEventListener("click", function () {
         startTextEl.textContent = " ";
         startBtn.textContent = "End Quiz";
         formEl.style.display = "block";
-        var submitBtn = document.createElement("button");
+        submitBtn = document.createElement("button");
         submitBtn.textContent = "Next";
         formEl.appendChild(submitBtn);
 
@@ -54,34 +54,151 @@ function timerUpMessage() {
 //startTime();
 
 var question = {
-    title: 'Question 1',
-    options: ['an', 'an', 'an', 'an'],
-    answer: 1
+    title: ['Question 1', 'Question 2', 'Question 3'],
+    options: (['an', 'an', 'an', 'an'], ['to', 'for', 'to', 'to']),
+    //answer: 1
 };
 
+var h2Div = document.getElementById('headerDiv')
+var answers = document.querySelectorAll('label');
+h2Div.textContent = question.title[0];
+answers.textContent = question.options[0]
+answers.forEach(function (element, index) {
+    element.textContent = question.options[index];
+})
+var length = question.title.length;
+//h2Div.textContent = question.title[0]
+//answers.textContent = question.options[0[0]]
 // show each question
 function seeQuestion(q) {
-    // select dom element headerDiv by id and assign to variable
-    var h2Div = document.getElementById('headerDiv');
-    // modify the text
-    h2Div.textContent = q.title;
-    //select all input elements with the same class
-    var answers = document.querySelectorAll('label');
+    //h2Div.textContent = q.title;
+    // var h2Div = document.getElementById('headerDiv')
+    // var answers = document.querySelectorAll('label');
+    // h2Div.textContent = q.title[0]
+    // answers.textContent = q.options[0[0]]
 
-    //console.log(option);
 
-    // for each of the answers, display text from the question object using iterations through the options array
-    answers.forEach(function (element, index) {
-        element.textContent = q.options[index];
 
-    });
-};
 
-var question2 = {
-    title: 'Question 2',
-    options: ['to', 'to', 'to', 'to'],
-    answer: 2
-};
+
+
+    submitBtn.addEventListener('click', function (event) {
+        //h2Div.textContent = question.title[0];
+        //answers.textContent = question.options[0]
+        for (var i = 0; i < question.title.length; i++) {
+            console.log(question.title[i])
+            event.preventDefault()
+            var nextq = q.title[i];
+            console.log(nextq)
+        }
+        for (var i = 0; i < q.options.length; i++) {
+            event.preventDefault();
+            var nextqa = q.options[i]
+            console.log(nextqa)
+
+        }
+
+
+        //q.title[i++]
+        // h2Div.textContent = q.title[i]
+        // answers.textContent = q.options[i]
+        // console.log("hi")
+
+        h2Div.textContent = nextq
+        answers.textContent = nextqa
+        console.log("hi")
+
+
+
+
+    })
+
+    //nextQuestion(q);
+
+
+}
+
+// submitBtn.addEventListener('click', function (event) {
+//     event.preventDefault();
+//nextQuestion(q);
+// for (var i = 0; i < q.title.length; i++) {
+//     h2Div.textContent = q.title[i]
+//     answers.textContent = q.options[i]
+
+//q.title[i]
+//q.answers[[i]]
+//nextQuestion();
+
+
+
+
+//answers.textContent = q.option
+
+// submitBtn.addEventListener('click', function (event, array) {
+//     event.preventDefault();
+//     for (var i = 0; i < q.title.length; i++) {
+//         //element = event.target;
+//         //var element = event.target;
+
+//         // select dom element headerDiv by id and assign to variable
+//         //var h2Div = document.getElementById('headerDiv');
+//         // modify the text
+//         //question.title[i]++;
+//         h2Div.textContent = q.title[i];
+
+
+//         //for (var i = 0; i < q.title.length; i++) {
+//         //select all input elements with the same class
+//         //var answers = document.querySelectorAll('label');
+//         //answers.textContent = q.options[i];
+
+//         //console.log(option);
+
+//         // for each of the answers, display text from the question object using iterations through the options array
+//         answers.forEach(function (element, index) {
+//             element.textContent = q.options[index];
+//         })
+
+
+
+
+//function nextQuestion(q) {
+// submitBtn.addEventListener('click', function (event) {
+//     event.preventDefault();
+// console.log("hi")
+// for (var i = 0; i < q.length; i++) {
+//     q.title[i++]
+//     h2Div.textContent = q.title[i]
+//     answers.textContent = q.options[i]
+//     seeQuestion(i++)
+//     //seeQuestion(i++);
+//     //seeQuestion(q);
+// }
+// ;
+
+
+
+// submitBtn.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     for (var i = 0; i < q.title.length; i++) {
+//         var h2Div = document.getElementById('headerDiv');
+//         h2Div.textContent = q.title[i];
+//         var answers = document.querySelectorAll('label');
+//         answers.textContent = q.options[i];
+//         q.answer[i];
+
+
+
+
+
+//     }
+// })
+
+// var question2 = {
+//     title: 'Question 2',
+//     options: ['to', 'to', 'to', 'to'],
+//     answer: 2
+// };
 
 
 //seeQuestion(question);
@@ -95,7 +212,7 @@ select.addEventListener("keyof", function () {
     } else {
         console.log('clicked')
     }
-})
+});
 
 // var select = document.getElementById('a1');
 // select.addEventListener('click', function () {
@@ -133,4 +250,3 @@ select.addEventListener("keyof", function () {
 // //this function will be executed every time their is a click on the button
 // nextBtn.addEventListener('click', function () {
 //     console.log('clicked next');
-
